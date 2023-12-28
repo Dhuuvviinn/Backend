5bcb196f0d4e3146277daf4dd5305c848484332e
DhruvinPATEL <dhruvinpatel17000@gmail.com>
2023-12-28 10:27:20 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const dotenv = require("dotenv")
+const app = express()
+dotenv.config()
+const Port = process.env.PORT
+
+app.get("/",(req,res)=>{
+  res.send("Hello world")
+})
+
+app.get("/google",(req,res)=>{
+    res.send("DhruvinPatel1700@gmail.com")
+  })
+  
+
+app.listen(Port,()=>{
+    console.log(`Server is Connected at http://localhost:${Port}`)
+})
